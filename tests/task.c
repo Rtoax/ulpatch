706875f573ae631c5839cd7ff01e39801dc5f8e4
Rong Tao <rongtao@cestc.cn>
2022-07-16 20:11:57 +0800

    Add tests: Task.open_failed and Task.open_non_exist
    $ ./tests/elftools_test  -V -f Task
    === OK       Elf_init.elf_main
    === OK       Elf_init.create_elf_client
    === OK       Elf_init.close_elf_client
    === OK       Elf_init.create_elf_client_again
    === OK       Task.open_free
    [ERROR][open_pid_mem:36] open /proc/1/mem failed. Permission denied
    === OK       Task.open_failed
    [ERROR][open_pid_mem:36] open /proc/-1/mem failed. No such file or directory
    === OK       Task.open_non_exist
    =========================================
    === Total 7 tested
    ===  Success 7
    ===  Failed 0

--- tests/task.c
+++ tests/task.c
@@ -14,3 +14,19 @@ TEST(Task,	open_free,	0)
 
 	return free_task(task);
 }
+
+TEST(Task,	open_failed,	-1)
+{
+	// Try to open pid 1 (systemd)
+	struct task *task = open_task(1);
+
+	return task?0:-1;
+}
+
+TEST(Task,	open_non_exist,	-1)
+{
+	// Try to open pid -1 (non exist)
+	struct task *task = open_task(-1);
+
+	return task?0:-1;
+}
