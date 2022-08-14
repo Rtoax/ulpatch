9637db5953f1ae20f35662aad5aac2cd677ed89c
Rong Tao <rongtao@cestc.cn>
2022-08-14 15:17:57 +0800

    [TODO] test libc.so

---
+++ tests/utils/libc.so.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright (C) 2022 Rong Tao */
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <utils/log.h>
+#include <utils/list.h>
+
+#include "../test_api.h"
+
+
+TEST(Libc,	TODO,	0)
+{
+	return 0;
+}
+
