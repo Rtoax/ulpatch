baa6b638649bcb51b444872e53f1ed78efda2c01
Rong Tao <rtoax@foxmail.com>
2022-09-20 22:19:41 +0800

    Pretty version info print
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- src/utils/version.c
+++ src/utils/version.c
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /* Copyright (C) 2022 Rong Tao */
+#include <stdio.h>
 #include "util.h"
+
+static const char *color[] = {
+	"\033[48;5;255m",
+	"\033[48;5;252m",
+	"\033[48;5;250m",
+	"\033[48;5;248m",
+	"\033[48;5;246m",
+	"\033[48;5;244m",
+	"\033[48;5;242m",
+	"\033[48;5;240m",
+	"\033[48;5;238m",
+	"\033[48;5;236m",
+	"\033[48;5;234m",
+	"\033[48;5;232m",
+};
 
 const char *upatch_arch(void)
 {
@@ -18,6 +34,13 @@ const char *upatch_version(void)
 #if !defined(UPATCH_VERSION)
 # error "Must define string UPATCH_VERSION"
 #endif
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(color); i++) {
+		printf("%s  %s", color[i], "\033[m");
+	}
+	printf("\n");
+
 	return UPATCH_VERSION;
 }
 
