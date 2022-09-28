a71732bd6a78e9218b4af5f8f9ad0ef0412d0e22
Rong Tao <rtoax@foxmail.com>
2022-09-28 19:41:13 +0800

    Detect binutils-devel
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

---
+++ cmake/find_binutils.cmake
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2022 Rong Tao
+#
+# - Try to find binutils
+# Once done this will define
+#
+#  BINUTILS_FOUND - system has binutils
+#  BINUTILS_INCLUDE_DIRS - the binutils include directory
+#  BINUTILS_BFD_H - the binutils has version.h header
+
+find_path(BINUTILS_INCLUDE_DIRS
+  NAMES
+    bfd.h
+  PATHS
+    ENV CPATH)
+
+include(FindPackageHandleStandardArgs)
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(binutils-devel "Please install the binutils-devel development package"
+  BINUTILS_INCLUDE_DIRS)
+
+SET(CMAKE_REQUIRED_LIBRARIES elf)
+INCLUDE(CheckCSourceCompiles)
+CHECK_C_SOURCE_COMPILES("
+#include <bfd.h>
+int main() {
+	return 0;
+}" BINUTILS_BFD_H)
+SET(CMAKE_REQUIRED_LIBRARIES)
+
+mark_as_advanced(BINUTILS_INCLUDE_DIRS BINUTILS_BFD_H)
+
