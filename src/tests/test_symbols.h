b14938c7ffccf45ac5614eede48ca23f8fc8fa4c
Rong Tao <rongtao@cestc.cn>
2022-10-01 14:21:04 +0800

    Some non static symbol may have special alias symbol
    (gdb) p stdout
    $1 = (FILE *) 0x7ffff7f9d780 <_IO_2_1_stdout_>
    
    I don't know how to get value 0x7ffff7f9d780 with 'stdout' symbol
    by parse ELF file, when i figure it out, i'll implement.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- src/tests/test_symbols.h
+++ src/tests/test_symbols.h
@@ -12,6 +12,17 @@ for (TEST_SYM_FOR_EACH_I = 0;
 
 /* Here start to define symbols */
 /* not constant */
+/* For example, if you try get 'stdout' address after process start running,
+ * the value you get from VMA is not equal to the value you got from target
+ * process directly. For 'stdout', you should get it's address from symbol
+ * '_IO_2_1_stdout_', the gdb output like:
+ *
+ * (gdb) p stdout
+ * $1 = (FILE *) 0x7ffff7f9d780 <_IO_2_1_stdout_>
+ *
+ * So, if we found the wrong value with symbol 'stdout', try '_IO_2_1_stdout_'
+ * again, maybe we can get what we want.
+ */
 TEST_SYM_NON_STATIC(stdin)
 TEST_SYM_NON_STATIC(stdout)
 TEST_SYM_NON_STATIC(stderr)
