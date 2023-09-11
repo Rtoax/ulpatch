c5c536055b5849c039ede99b9bbd0a809247b403
Rong Tao <rongtao@cestc.cn>
2023-09-11 21:15:40 +0800

    Add BUILD_UTASK decides whether to compile utask command.
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- docs/compile.sh
+++ docs/compile.sh
@@ -4,6 +4,6 @@ How to Compile?
 ```bash
 $ mkdir build
 $ cd build
-$ cmake -DBUILD_TESTING=OFF -DBUILD_UFTRACE=OFF ..
+$ cmake -DBUILD_TESTING=OFF -DBUILD_UFTRACE=OFF -DBUILD_UTASK=OFF ..
 $ make -j$(npro)
 ```
