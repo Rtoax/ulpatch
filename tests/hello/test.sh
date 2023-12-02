a92964d52aedad81ad1ad628182193ec442aa3ea
Rong Tao <rtoax@foxmail.com>
2023-12-02 13:32:45 +0800

    tests/hello: test.sh: Dump all live patches to file
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- tests/hello/test.sh
+++ tests/hello/test.sh
@@ -8,3 +8,10 @@ make
 
 upatch -p ${pid} --patch patch.up --log-level=9
 cat /proc/${pid}/maps
+
+patches_addr_range=( $(cat /proc/${pid}/maps | grep patch- | awk '{print $1}') )
+
+for ((i = 0; i < ${#patches_addr_range[@]}; i++))
+do
+	utask -p ${pid} --dump-vma ${patches_addr_range[$i]} -o patch-$i.up
+done
