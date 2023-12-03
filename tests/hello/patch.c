256b721a6c2803634e89c214526778e252c753cd
Rong Tao <rtoax@foxmail.com>
2023-12-03 18:00:44 +0800

    tests/hello: Remove print_hello to upatch_print_hello
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- tests/hello/patch.c
+++ tests/hello/patch.c
@@ -2,10 +2,10 @@
 #include <upatch/meta.h>
 
 
-void print_hello(void)
+void upatch_print_hello(void)
 {
 	// TODO: Not support library function yet
 	//printf("Hello World. Patched\n");
 }
 
-UPATCH_INFO(upatch, print_hello, print_hello, "Rong Tao");
+UPATCH_INFO(upatch, upatch_print_hello, print_hello, "Rong Tao");
