d5287d3a743a3417780324db6d4a892db95fa96f
Rong Tao <rtoax@foxmail.com>
2023-12-23 14:48:16 +0800

    {tests/hello,TODO}: Should support multi-thread
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- tests/hello/hello.c
+++ tests/hello/hello.c
@@ -2,9 +2,22 @@
 /* Copyright (C) 2022-2023 Rong Tao <rtoax@foxmail.com> */
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <signal.h>
 
-
+static sig_atomic_t keep_running = true;
 static unsigned long count = 0;
+
+void sig_handler(int sig)
+{
+	switch (sig) {
+	case SIGINT:
+		printf("Catch ctrl-C.\n");
+		keep_running = false;
+		break;
+	}
+}
 
 void internal_print_hello(unsigned long ul)
 {
@@ -16,19 +29,36 @@ void print_hello(unsigned long ul)
 	internal_print_hello(ul);
 }
 
-int main(int argc, char *argv[])
+void *routine(void *arg)
 {
 	unsigned long ul = 0xff;
+
+	while (keep_running) {
+		print_hello(ul);
+		count++;
+		sleep(2);
+	}
+	return NULL;
+}
+
+#define NR_THREADS	3
+
+int main(int argc, char *argv[])
+{
+	int i;
+	pthread_t threads[NR_THREADS];
+
+	signal(SIGINT, sig_handler);
 
 #define PRINT_ADDR(a)	printf("%-32s: %#016x\n", #a, a);
 	PRINT_ADDR(print_hello);
 	PRINT_ADDR(puts);
 
-	while (1) {
-		print_hello(ul);
-		count++;
-		sleep(2);
-	}
+	for (i = 0; i < NR_THREADS; i++)
+		pthread_create(&threads[i], NULL, routine, NULL);
+
+	for (i = 0; i < NR_THREADS; i++)
+		pthread_join(threads[i], NULL);
 
 	return 0;
 }
