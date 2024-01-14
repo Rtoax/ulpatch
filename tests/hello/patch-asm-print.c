b9d4c1e1690e35143151a1f05f5ad9b655e2f56a
Rong Tao <rongtao@cestc.cn>
2024-01-14 14:29:51 +0800

    tests/hello/patch-asm-print.c: Support aarch64
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tests/hello/patch-asm-print.c
+++ tests/hello/patch-asm-print.c
@@ -8,7 +8,7 @@
 void ulpatch_print_hello_exit(unsigned long ul)
 {
 #if defined(__x86_64__)
-	/* write("Hello") */
+	/* write("Hello\n") */
 	asm(
 		"mov    $0x1,%al\n"
 		"mov    %al,%dil\n"
@@ -19,6 +19,33 @@ void ulpatch_print_hello_exit(unsigned long ul)
 		"syscall\n"
 		"pop    %rsi\n"
 		"pop    %rsi\n"
+	);
+#elif defined(__aarch64__)
+	/* write("Hello\n") */
+	asm(
+		"stp	x29, x30, [sp, #-32]!\n"
+		"mov	x29, sp\n"
+		"str	xzr, [sp, #16]\n"
+		"mov	w0, #0x48\n"
+		"strb	w0, [sp, #16]\n"
+		"mov	w0, #0x65\n"
+		"strb	w0, [sp, #17]\n"
+		"mov	w0, #0x6c\n"
+		"strb	w0, [sp, #18]\n"
+		"mov	w0, #0x6c\n"
+		"strb	w0, [sp, #19]\n"
+		"mov	w0, #0x6f\n"
+		"strb	w0, [sp, #20]\n"
+		"mov	w0, #0xa\n"
+		"strb	w0, [sp, #21]\n"
+		"add	x0, sp, #0x10\n"
+		"str	x0, [sp, #24]\n"
+		"\n"
+		"mov	x0, #1\n"
+		"ldr	x1, [sp, #24]\n"
+		"mov	x2, #0x8\n"
+		"mov	w8, #64\n"
+		"svc	#0\n"
 	);
 #else
 # warning Not supported CPU architecture yet.
