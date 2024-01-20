06f69f84e1b1c0c58353ad66a1f1a07eb1c6aee2
Rong Tao <rongtao@cestc.cn>
2024-01-20 13:58:34 +0800

    tests/hello: patch-asm-puts.c: aarch64: Call puts("Hello")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tests/hello/patch-asm-puts.c
+++ tests/hello/patch-asm-puts.c
@@ -13,8 +13,35 @@ void ulpatch_print_hello(unsigned long ul)
 	"mov    %rsp,%rdi\n"
 	"call   puts\n"
 	"pop    %rsi\n");
+#elif defined(__aarch64__)
+	asm(
+	"stp	x29, x30, [sp, #-32]!\n"
+	"mov	x29, sp\n"
+	"mov	w0, #0x48\n"                  	// #72
+	"strb	w0, [sp, #24]\n"
+	"mov	w0, #0x65\n"                  	// #101
+	"strb	w0, [sp, #25]\n"
+	"mov	w0, #0x6c\n"                  	// #108
+	"strb	w0, [sp, #26]\n"
+	"mov	w0, #0x6c\n"                  	// #108
+	"strb	w0, [sp, #27]\n"
+	"mov	w0, #0x6f\n"                  	// #111
+	"strb	w0, [sp, #28]\n"
+	"add	x0, sp, #0x18\n"
+	"bl	puts\n"
+	"mov	w0, #0x0\n"                   	// #0
+	"ldp	x29, x30, [sp], #32\n");
 #else
 # warning Not supported CPU architecture yet.
 #endif
 }
 ULPATCH_INFO(ulpatch_print_hello, print_hello, "Rong Tao");
+
+/* Only for test */
+#ifdef TEST_MAIN
+int main(void)
+{
+	ulpatch_print_hello(3);
+	return 0;
+}
+#endif
