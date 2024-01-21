349bf720329e5babd8f2988f6e4b0bcb76090fa3
Rong Tao <rongtao@cestc.cn>
2024-01-21 18:43:43 +0800

    tests/dlfcn: Just could not found internal_print_hello from dlopen
        rongtao@rtoax:~/Git/ulpatch/tests/dlfcn$ ./hello
        print_hello                     : 0x0000000040127e
        puts                            : 0x00000000401030
        Hello World. 0, 255, 40127e, 7fdeaec1d143
        Hello World. 1, 255, 40127e, 7fdeaec1d143
        Hello World. 2, 255, 40127e, 7fdeaec1d143
        Hello World. Patched   <
        Hello World. Patched
        Hello World. Patched
        ./hello: symbol lookup error: ./patch.so: undefined symbol: internal_print_hello
                                                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
        $ sudo ultask -p $(pidof hello) --jmp-from 0x40127e --jmp-to 7fdeaec1d143
    
    Fix: commit a46c1aa51d3a ("ultask: Add --jmp-{from,to} arguments (todo)")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tests/dlfcn/patch.c
+++ tests/dlfcn/patch.c
@@ -5,6 +5,11 @@ void internal_print_hello(unsigned long ul);
 static void patch_internal_print_hello(unsigned long ul)
 {
 	printf("Hello World. Patched\n");
+	/**
+	 * Dynamic libraries after dlopen cannot reference symbols in the
+	 * original process, but can they be used in other libraries?
+	 * I doubt it.
+	 */
 	internal_print_hello(ul);
 }
 
