c2c609772fd5a0c61974df44381e6979955f84f0
Rong Tao <rongtao@cestc.cn>
2024-01-21 18:56:17 +0800

    tests/dlfcn/patch.c: Add TEST_TARGET_SYMBOL macro
    Maybe I can't solve the problem described in commit 349bf720329e ("tests/dlfcn:
    Just could not found internal_print_hello from dlopen")
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tests/dlfcn/patch.c
+++ tests/dlfcn/patch.c
@@ -2,15 +2,22 @@
 
 void internal_print_hello(unsigned long ul);
 
+static int a;
+static char *s = "Hello";
+
 static void patch_internal_print_hello(unsigned long ul)
 {
-	printf("Hello World. Patched\n");
+	a++;
+	printf("Hello World. %s Patched %d\n", s, a);
+
+#if defined(TEST_TARGET_SYMBOL)
 	/**
 	 * Dynamic libraries after dlopen cannot reference symbols in the
 	 * original process, but can they be used in other libraries?
 	 * I doubt it.
 	 */
 	internal_print_hello(ul);
+#endif
 }
 
 void patch_print(unsigned long ul)
