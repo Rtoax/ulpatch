299db3596079c4a1ae976662705117e4b37fd2fa
Rong Tao <rongtao@cestc.cn>
2024-02-04 14:12:31 +0800

    tests/dlfcn/pdlsym_mine.c: Load PT_DYNAMIC
        $ ./pdlsym_tst -p $(pidof hello) -a 0x00400000  -s print_hello
        handle a program header 6
        handle a program header 3
        handle a program header 1
        handle a program header 1
        handle a program header 1
        handle a program header 1
        handle a program header 2
        tag = 403e08
        DT_STRTAB
        DT_SYMTAB
        DT_STRSZ
        DT_SYMENT
        handle a program header 4
        handle a program header 4
        handle a program header 1685382483
        handle a program header 1685382480
        handle a program header 1685382481
        handle a program header 1685382482
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tests/dlfcn/pdlsym_mine.c
+++ tests/dlfcn/pdlsym_mine.c
@@ -14,6 +14,7 @@ struct pelf {
 	Elf64_Ehdr ehdr;
 	int mem_fd;
 	unsigned long base_addr;
+	off_t strtab, symtab, strsz, syment;
 };
 
 static int readp(int mem_fd, off_t addr, void *buf, size_t len)
@@ -33,13 +34,15 @@ struct pelf *openp(pid_t pid, off_t base)
 	uint32_t magic;
 	Elf64_Ehdr ehdr;
 	struct pelf *pelf;
-
+	off_t strtab, symtab, strsz, syment;
 
 	pelf = malloc(sizeof(struct pelf));
 	if (!pelf) {
 		errno = -ENOMEM;
 		return NULL;
 	}
+
+	strtab = symtab = strsz = syment = 0;
 
 	sprintf(proc_mem, "/proc/%d/mem", pid);
 	mem_fd = open(proc_mem, O_RDONLY);
@@ -68,33 +71,87 @@ struct pelf *openp(pid_t pid, off_t base)
 			fprintf(stderr, "Fail to read phdr.\n");
 			goto fatal;
 		}
-		if (phdr.p_type == PT_NULL || phdr.p_type != PT_LOAD ||
+
+		if (phdr.p_type == PT_NULL && phdr.p_type != PT_LOAD &&
 		    phdr.p_type != PT_DYNAMIC) {
 			continue;
 		}
 
+		fprintf(stderr, "handle a program header %d\n", phdr.p_type);
+
 		off_t offset = phdr.p_offset;
 		off_t vaddr = phdr.p_vaddr;
 		off_t filesz = phdr.p_filesz;
-		off_t memdz = phdr.p_memsz;
+		off_t memsz = phdr.p_memsz;
 
-		switch (phdr.p_type) {
-		case PT_LOAD:
+		if (phdr.p_type == PT_LOAD) {
 			if (ehdr.e_type == ET_EXEC) {
 				if (vaddr - offset < base) {
 					errno = -EFAULT;
 					goto fatal;
 				}
 			}
-			break;
-		case PT_DYNAMIC:
-			break;
+		} else if (phdr.p_type == PT_DYNAMIC) {
+			int id;
+			Elf64_Dyn dyn;
+			off_t tag = (ehdr.e_type == ET_EXEC ? 0 : base) + vaddr;
+			fprintf(stderr, "tag = %lx\n", tag);
+			for (id = 0; id * sizeof(Elf64_Dyn) < memsz; id++) {
+				ret = readp(mem_fd,
+					    tag + id * sizeof(Elf64_Dyn),
+					    &dyn,
+					    sizeof(Elf64_Dyn));
+				if (ret < sizeof(Elf64_Dyn)) {
+					fprintf(stderr, "Failed read dyn.\n");
+					goto fatal;
+				}
+				switch (dyn.d_tag) {
+				case DT_STRTAB:
+					fprintf(stderr, "DT_STRTAB\n");
+					strtab = dyn.d_un.d_val;
+					break;
+				case DT_SYMTAB:
+					fprintf(stderr, "DT_SYMTAB\n");
+					symtab = dyn.d_un.d_val;
+					break;
+				case DT_STRSZ:
+					fprintf(stderr, "DT_STRSZ\n");
+					strsz = dyn.d_un.d_val;
+					break;
+				case DT_SYMENT:
+					fprintf(stderr, "DT_SYMENT\n");
+					syment = dyn.d_un.d_val;
+					break;
+				}
+			}
 		}
+	}
+
+	/* Check that we have all program headers required for dynamic linking */
+	if (!strtab || !symtab || !strsz || !syment) {
+		fprintf(stderr, "No found DT_\n");
+		goto fatal;
+	}
+
+	/* String table (immediately) follows the symbol table */
+	if (symtab >= strtab) {
+		fprintf(stderr, "symtab >= strtab\n");
+		goto fatal;
+	}
+
+	/* Symbol entry size is a non-zero integer that divides symtab size */
+	if ((strtab - symtab) % syment) {
+		fprintf(stderr, "(strtab - symtab) %% syment != 0\n");
+		goto fatal;
 	}
 
 	pelf->pid = pid;
 	pelf->mem_fd = mem_fd;
 	memcpy(&pelf->ehdr, &ehdr, sizeof(Elf64_Ehdr));
+	pelf->strtab = strtab;
+	pelf->symtab = symtab;
+	pelf->strsz = strsz;
+	pelf->syment = syment;
 
 	return pelf;
 fatal:
