d68e6b1808ed9db106b25597879424ab52b81776
Rong Tao <rongtao@cestc.cn>
2024-04-02 14:07:30 +0800

    tests/hello: patch-add-vars.c: Add local_l var
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tests/hello/patch-add-vars.c
+++ tests/hello/patch-add-vars.c
@@ -6,6 +6,7 @@
 
 
 int local_i = 123;
+long int local_l = 123;
 static long static_i = 1024;
 char *local_s = "Dear";
 static char *static_s = "you";
@@ -15,10 +16,12 @@ void ulp_add_var(unsigned long ul)
 	int i, a = 10;
 
 	local_i++;
+	local_l++;
 	static_i++;
 	static_i += 2;
 
-	printf("Hello World. Patched %ld %d, %d, %d\n", ul, local_i, static_i, a);
+	printf("Hello World. Patched %ld %d, %ld, %d, %d\n",
+		ul, local_i, local_l, static_i, a);
 	printf("%s %s\n", local_s, static_s);
 
 	for (i = 0; i < 3; i++)
