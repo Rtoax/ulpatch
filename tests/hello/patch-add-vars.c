136d93d565006c96a5b0e7a319588d947973aacd
Rong Tao <rongtao@cestc.cn>
2024-04-17 17:14:40 +0800

    tests/hello: add-vars for PIC ulp (todo: segfault)
        $ ./compile-pic.sh NOSTATIC=1                           1 (compile)
    
        $ ./test.sh -u patch-add-vars.ulp                       3 (patch)
    
        $ ../tests/hello/hello-pie                              2 (exec)
        print_hello                     : 0x00000028f5c1e8
        puts                            : 0x0000007057a390
        Hello World. 0, 255
        Hello World. 1, 255
        Hello World. 2, 255
        Hello World. 3, 255
        Hello World. 3, 255
        Hello World. 3, 255
        Segmentation fault (core dumped)                        4 (segfault)
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tests/hello/patch-add-vars.c
+++ tests/hello/patch-add-vars.c
@@ -7,9 +7,11 @@
 
 int local_i = 123;
 long int local_l = 123;
-static long static_i = 1024;
 char *local_s = "Dear";
+#if !defined(NOSTATIC)
+static long static_i = 1024;
 static char *static_s = "you";
+#endif
 
 void ulp_add_var(unsigned long ul)
 {
@@ -17,16 +19,24 @@ void ulp_add_var(unsigned long ul)
 
 	local_i++;
 	local_l++;
+#if !defined(NOSTATIC)
 	static_i++;
 	static_i += 2;
+#endif
 
 #if !defined(NOLIBC)
-	printf("Hello World. Patched %ld %d, %ld, %d, %d\n",
-		ul, local_i, local_l, static_i, a);
-	printf("%s %s\n", local_s, static_s);
+	printf("Hello World. Patched L: %d, %ld, %s\n", local_i, local_l, local_s);
+	printf("Hello World. Patched F: %ld %d\n", ul, a);
 
 	for (i = 0; i < 3; i++)
-		printf("- %d -\n", i);
+		printf("%d\n", i);
+
+	/* FIXME: This will segfault. Why? */
+	//printf("\n");
+
+# if !defined(NOSTATIC)
+	printf("Hello World. Patched S: %d %s\n", static_i, static_s);
+# endif
 #endif
 }
 ULPATCH_INFO(ulp_add_var, print_hello, "Rong Tao");
