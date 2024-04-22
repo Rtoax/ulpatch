543eb89b845e5e5175854b398164099512dfefb8
Rong Tao <rongtao@cestc.cn>
2024-04-22 19:43:47 +0800

    archive.sh: Check .git/ exist or not.
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- archive.sh
+++ archive.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-version=$(git describe --abbrev=0 --tags 2>/dev/null)
-
-git archive --format tar.gz --prefix=ulpatch-${version}/ --output ulpatch-${version}.tar.gz master
+if [[ -d .git/ ]]; then
+	version=$(git describe --abbrev=0 --tags 2>/dev/null)
+	git archive --format tar.gz --prefix=ulpatch-${version}/ --output ulpatch-${version}.tar.gz master
+else
+	echo "ERROR: You are not in git tree."
+	exit 1
+fi
