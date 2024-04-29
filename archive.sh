850ade6688a935c2a1e7fbc6b60407c878ccc705
Rong Tao <rongtao@cestc.cn>
2024-04-29 15:53:32 +0800

    archive.sh: Fix code line order
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- archive.sh
+++ archive.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ -d .git/ ]]; then
-	version=$(git describe --abbrev=0 --tags 2>/dev/null)
-	git archive --format tar.gz --prefix=ulpatch-${version}/ --output ulpatch-${version}.tar.gz master
-else
+if [[ ! -d .git/ ]]; then
 	echo "ERROR: You are not in git tree."
 	exit 1
 fi
+
+version=$(git describe --abbrev=0 --tags 2>/dev/null)
+git archive --format tar.gz --prefix=ulpatch-${version}/ --output ulpatch-${version}.tar.gz master
