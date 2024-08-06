6f1df0e9291e96e0a661175d5e26385431f00c76
Rong Tao <rongtao@cestc.cn>
2024-08-06 09:10:43 +0800

    ulpatch_test: Add Asm.{sleep,write} tests
        $ ./src/tests/ulpatch_test -f Asm. -v --lv dbg
        =========================================
        ===
        === ULPatch Testing
        ===
        ===  version: v0.5.7-121-g8e4a
        === ---------------------------
        ===   22/76   Asm.sleep
        1000420us OK
        ===   23/76   Asm.write
        Hello-
        Hello
        5us OK
        =========================================
        === Total 2 tested
        ===  Success 2
        ===  Failed 0
        ===  Spend 1000ms 500.21ms/per
        =========================================
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ src/tests/patch/asm.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright (C) 2024 Rong Tao <rtoax@foxmail.com> */
+#include <errno.h>
+
+#include <utils/log.h>
+#include <utils/util.h>
+#include <patch/asm.h>
+
+#include <tests/test_api.h>
+
+TEST(Asm, sleep, 0)
+{
+	ASM_SLEEP(1);
+	return 0;
+}
+
+TEST(Asm, write, 0)
+{
+	char msg[] = {"Hello-\n"};
+	int len = 7;
+	ASM_WRITE(1, msg, len);
+	ASM_WRITE_HELLO();
+	return 0;
+}
+
