2f09b48a092e0420ab8fd7d2e7bf8be4bd413d55
Rong Tao <rongtao@cestc.cn>
2024-08-07 19:10:41 +0800

    utils: Add disasm.{c,h}
    Use capstone as disassembler engine.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ src/utils/disasm.c
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright (C) 2022-2024 Rong Tao <rtoax@foxmail.com> */
+#include <utils/util.h>
+#include <utils/disasm.h>
+
---
+++ src/utils/disasm.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright (C) 2024 Rong Tao <rtoax@foxmail.com> */
+
+#if defined(HAVE_CAPSTONE_CAPSTONE_H)
+#include <capstone/capstone.h>
+#endif
+
+
