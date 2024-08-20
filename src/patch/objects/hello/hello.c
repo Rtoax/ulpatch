1c630809a7987b8db5135fff25a25692da78895e
Rong Tao <rongtao@cestc.cn>
2024-08-20 21:25:38 +0800

    objects/hello: Apply ulp-config
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- src/patch/objects/hello/hello.c
+++ src/patch/objects/hello/hello.c
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /* Copyright (C) 2022-2024 Rong Tao */
+#ifndef __ULP_DEV
+#define __ULP_DEV
+#endif
 #include <stdio.h>
-#include <patch/patch.h>
-
-/* TODO */
+#include <patch/asm.h>
+#include <patch/meta.h>
 
 void new_hello_world(void)
 {
 	printf("Hello World from ulpatch.\n");
 }
-
 ULPATCH_INFO(new_hello_world, hello_world, "Rong Tao");
 
