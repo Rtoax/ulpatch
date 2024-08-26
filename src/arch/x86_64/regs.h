33b244d141ce52243b39885ee2bb8b8e23be2cd4
Rong Tao <rongtao@cestc.cn>
2024-08-26 20:28:14 +0800

    {aarch64,x86_64}/regs.h: Code style
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- src/arch/x86_64/regs.h
+++ src/arch/x86_64/regs.h
@@ -5,14 +5,15 @@
 #include <sys/user.h>
 #include <sys/syscall.h>
 
+#define SYSCALL_REGS_PREPARE(regs, nr, p1, p2, p3, p4, p5, p6) do {	\
+		regs.rax = (unsigned long)nr;	\
+		regs.rdi = p1;	\
+		regs.rsi = p2;	\
+		regs.rdx = p3;	\
+		regs.r10 = p4;	\
+		regs.r8 = p5;	\
+		regs.r9 = p6;	\
+	} while (0)
 
-#define SYSCALL_REGS_PREPARE(regs, nr, p1, p2, p3, p4, p5, p6)  \
-        regs.rax = (unsigned long)nr;   \
-        regs.rdi = p1;  \
-        regs.rsi = p2;  \
-        regs.rdx = p3;  \
-        regs.r10 = p4;  \
-        regs.r8 = p5;   \
-        regs.r9 = p6;
-#define SYSCALL_RET(regs)   regs.rax
-#define SYSCALL_IP(regs)    regs.rip
+#define SYSCALL_RET(regs)	regs.rax
+#define SYSCALL_IP(regs)	regs.rip
