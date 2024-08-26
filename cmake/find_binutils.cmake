14abe663eb6b5f280ef6498ad191ca89e6d5e874
Rong Tao <rongtao@cestc.cn>
2024-08-26 15:08:20 +0800

    cmake: bfd: Must support bfd_elf_bfd_from_remote_memory()
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- cmake/find_binutils.cmake
+++ cmake/find_binutils.cmake
@@ -6,7 +6,8 @@
 #
 #  BINUTILS_FOUND - system has binutils
 #  BINUTILS_INCLUDE_DIRS - the binutils include directory
-#  BINUTILS_BFD_H - the binutils has version.h header
+#  BINUTILS_BFD_H - the binutils has bfd.h header
+#  BINUTILS_HAVE_BFD_ELF_BFD_FROM_REMOTE_MEMORY - support bfd_elf_bfd_from_remote_memory()
 
 find_path(BINUTILS_INCLUDE_DIRS
 	NAMES bfd.h
@@ -24,18 +25,27 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(binutils-devel "Please install the binutils de
 	BINUTILS_BFD_LIBRARIES
 	BINUTILS_INCLUDE_DIRS)
 
-SET(CMAKE_REQUIRED_LIBRARIES elf)
+SET(CMAKE_REQUIRED_LIBRARIES elf bfd)
 INCLUDE(CheckCSourceCompiles)
 CHECK_C_SOURCE_COMPILES("
 #include <bfd.h>
 int main(void) {
 	return 0;
 }" BINUTILS_BFD_H)
+CHECK_C_SOURCE_COMPILES("
+#include <bfd.h>
+int main(void) {
+	struct bfd *bfd, *templ;
+	templ = bfd_openr(\"/bin/ls\", NULL);
+	bfd = bfd_elf_bfd_from_remote_memory(templ, 0, 0, NULL, NULL);
+	return 0;
+}" BINUTILS_HAVE_BFD_ELF_BFD_FROM_REMOTE_MEMORY)
 SET(CMAKE_REQUIRED_LIBRARIES)
 
 mark_as_advanced(
 	BINUTILS_INCLUDE_DIRS
 	BINUTILS_BFD_LIBRARIES
 	BINUTILS_BFD_H
+	BINUTILS_HAVE_BFD_ELF_BFD_FROM_REMOTE_MEMORY
 )
 
