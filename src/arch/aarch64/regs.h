48ce5081b3bf47799fc8d5f6798bf74dc77c385c
Rong Tao <rongtao@cestc.cn>
2024-08-27 09:50:51 +0800

    aarch64/regs.h: Fix error: ‘struct user_regs_struct’ has no member named ‘syscall_regs’
    [ 14%] Building C object src/utils/CMakeFiles/ulpatch_utils.dir/task-syscall.c.o
    In file included from /home/rongtao/Git/ulpatch/src/utils/task-syscall.c:25:
    /home/rongtao/Git/ulpatch/src/utils/task-syscall.c: In function ‘task_syscall’:
    /home/rongtao/Git/ulpatch/src/utils/../arch/aarch64/regs.h:10:21: error: ‘struct user_regs_struct’ has no member named ‘syscall_regs’
       10 |                 regs.regs[8] = (unsigned long)nr;       \
          |                     ^
    /home/rongtao/Git/ulpatch/src/utils/task-syscall.c:147:9: note: in expansion of macro ‘SYSCALL_REGS_PREPARE’
      147 |         SYSCALL_REGS_PREPARE(syscall_regs, nr, arg1, arg2, arg3, arg4, arg5, arg6);
    
    Fix: commit 33b244d141ce ("{aarch64,x86_64}/regs.h: Code style")
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- src/arch/aarch64/regs.h
+++ src/arch/aarch64/regs.h
@@ -6,15 +6,15 @@
 #include <sys/syscall.h>
 
 
-#define SYSCALL_REGS_PREPARE(regs, nr, p1, p2, p3, p4, p5, p6) do {	\
-		regs.regs[8] = (unsigned long)nr;	\
-		regs.regs[0] = p1;	\
-		regs.regs[1] = p2;	\
-		regs.regs[2] = p3;	\
-		regs.regs[3] = p4;	\
-		regs.regs[4] = p5;	\
-		regs.regs[5] = p6;	\
+#define SYSCALL_REGS_PREPARE(_regs, nr, p1, p2, p3, p4, p5, p6) do {	\
+		_regs.regs[8] = (unsigned long)nr;	\
+		_regs.regs[0] = p1;	\
+		_regs.regs[1] = p2;	\
+		_regs.regs[2] = p3;	\
+		_regs.regs[3] = p4;	\
+		_regs.regs[4] = p5;	\
+		_regs.regs[5] = p6;	\
 	} while (0)
 
-#define SYSCALL_RET(regs)	regs.regs[0]
-#define SYSCALL_IP(regs)	regs.pc
+#define SYSCALL_RET(_regs)	_regs.regs[0]
+#define SYSCALL_IP(_regs)	_regs.pc
