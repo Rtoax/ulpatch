75d1f104666ed99f9d8b26c6bff4cacec3914c91
Rong Tao <rtoax@foxmail.com>
2024-08-31 15:32:06 +0800

    test: Add Asm.exit
        $ ./src/tests/ulpatch_test -f Asm.exit -v
    
    Signed-off-by: Rong Tao <rtoax@foxmail.com>

--- src/tests/patch/asm.c
+++ src/tests/patch/asm.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /* Copyright (C) 2024 Rong Tao */
 #include <errno.h>
+#include <sys/wait.h>
 
 #include <utils/log.h>
 #include <utils/util.h>
@@ -23,3 +24,20 @@ TEST(Asm, write, 0)
 	return 0;
 }
 
+TEST(Asm, exit, 0)
+{
+	int pid, status;
+	int ret = 0;
+#define EXIT_VAL	0xff
+
+	pid = fork();
+	if (pid == 0)
+		ASM_EXIT(EXIT_VAL);
+	else {
+		waitpid(pid, &status, 0);
+		if (WEXITSTATUS(status) != EXIT_VAL)
+			ret = 1;
+	}
+
+	return ret;
+}
