480c6e65db33cd03a015f376375a4d1216daf27f
Rong Tao <rongtao@cestc.cn>
2024-09-14 09:27:21 +0800

    utils/task: Move some apis to single source code file
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ src/utils/task-current.c
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright (C) 2022-2024 Rong Tao */
+#include <stdio.h>
+#include <errno.h>
+#include <malloc.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <elf.h>
+#include <dirent.h>
+
+#include <elf/elf-api.h>
+
+#include <utils/log.h>
+#include <utils/task.h>
+
+
+/**
+ * We should implement the current macro because sometimes we cannot directly
+ * pass the task_struct structure as a function parameter. At the same time,
+ * this can also be unified with the kernel.
+ */
+static struct task_struct *current_task = NULL;
+static struct task_struct fallback_task = {
+	.pid = 0,
+};
+
+int set_current_task(struct task_struct *task)
+{
+	if (!task)
+		return -EINVAL;
+	current_task = task;
+	return 0;
+}
+
+void reset_current_task(void)
+{
+	current_task = NULL;
+}
+
+struct task_struct *const get_current_task(void)
+{
+	if (!current_task) {
+		lerror("Not set current task.\n");
+		errno = ENOENT;
+		/**
+		 * To prevent segmentation errors, NULL cannot be returned.
+		 */
+		return &fallback_task;
+	}
+	return current_task;
+}
+
---
+++ src/utils/task-proc.c
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright (C) 2022-2024 Rong Tao */
+#include <stdio.h>
+#include <errno.h>
+#include <malloc.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <elf.h>
+#include <dirent.h>
+
+#include <elf/elf-api.h>
+
+#include <utils/log.h>
+#include <utils/task.h>
+
+
+int open_pid_maps(pid_t pid)
+{
+	int mapsfd;
+	char maps[] = "/proc/1234567890/maps";
+
+	snprintf(maps, sizeof(maps), "/proc/%d/maps", pid);
+	mapsfd = open(maps, O_RDONLY);
+	if (mapsfd <= 0) {
+		lerror("open %s failed. %s\n", maps, strerror(errno));
+		mapsfd = -1;
+	}
+	return mapsfd;
+}
+
+int fprint_pid_maps(FILE *fp, int mapsfd)
+{
+	char ch;
+	int cnt = 0;
+
+	if (fp == NULL)
+		fp = stdout;
+
+	while (read(mapsfd, &ch, 1) == 1) {
+		fputc(ch, fp);
+		cnt++;
+	}
+	return cnt;
+}
+
+int __open_pid_mem(pid_t pid, int flags)
+{
+	char mem[] = "/proc/1234567890/mem";
+	snprintf(mem, sizeof(mem), "/proc/%d/mem", pid);
+	int memfd = open(mem, flags);
+	if (memfd <= 0) {
+		lerror("open %s failed. %s\n", mem, strerror(errno));
+		memfd = -errno;
+	}
+	return memfd;
+}
+
+int open_pid_mem_ro(pid_t pid)
+{
+	return __open_pid_mem(pid, O_RDONLY);
+}
+
+int open_pid_mem_rw(pid_t pid)
+{
+	return __open_pid_mem(pid, O_RDWR);
+}
+
