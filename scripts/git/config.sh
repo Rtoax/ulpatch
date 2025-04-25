f10e39116c9430ec7df03004f25fad4a7e4305ff
Rong Tao <rongtao@cestc.cn>
2025-04-25 23:15:44 +0800

    scripts: Add copyright comment
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- scripts/git/config.sh
+++ scripts/git/config.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# SPDX-License-Identifier: GPL-2.0-or-later
+set -e
+
 git config core.hooksPath scripts/git/hooks/ 2>/dev/null
 # This script called in top Makefile, need this 'get'.
 git config get core.hooksPath 2>/dev/null || {
