7fc3fb17279bff99db55e26e6396470d4808ab71
Rong Tao <rongtao@cestc.cn>
2025-05-06 16:37:04 +0800

    tests/hello: patch-asm-puts.c: modify indent
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tests/hello/patch-asm-puts.c
+++ tests/hello/patch-asm-puts.c
@@ -15,23 +15,22 @@ void ulp_asm_puts(unsigned long ul)
 		"pop %rax\n"
 		"pop %rax\n");
 #elif defined(__aarch64__)
-	asm(
-	"stp	x29, x30, [sp, #-32]!\n"
-	"mov	x29, sp\n"
-	"mov	w0, #0x48\n"                  	// #72
-	"strb	w0, [sp, #24]\n"
-	"mov	w0, #0x65\n"                  	// #101
-	"strb	w0, [sp, #25]\n"
-	"mov	w0, #0x6c\n"                  	// #108
-	"strb	w0, [sp, #26]\n"
-	"mov	w0, #0x6c\n"                  	// #108
-	"strb	w0, [sp, #27]\n"
-	"mov	w0, #0x6f\n"                  	// #111
-	"strb	w0, [sp, #28]\n"
-	"add	x0, sp, #0x18\n"
-	"bl	puts\n"
-	"mov	w0, #0x0\n"                   	// #0
-	"ldp	x29, x30, [sp], #32\n");
+	__asm__("stp	x29, x30, [sp, #-32]!\n"
+		"mov	x29, sp\n"
+		"mov	w0, #0x48\n"                  	// #72
+		"strb	w0, [sp, #24]\n"
+		"mov	w0, #0x65\n"                  	// #101
+		"strb	w0, [sp, #25]\n"
+		"mov	w0, #0x6c\n"                  	// #108
+		"strb	w0, [sp, #26]\n"
+		"mov	w0, #0x6c\n"                  	// #108
+		"strb	w0, [sp, #27]\n"
+		"mov	w0, #0x6f\n"                  	// #111
+		"strb	w0, [sp, #28]\n"
+		"add	x0, sp, #0x18\n"
+		"bl	puts\n"
+		"mov	w0, #0x0\n"                   	// #0
+		"ldp	x29, x30, [sp], #32\n");
 #else
 # warning Not supported CPU architecture yet.
 #endif
