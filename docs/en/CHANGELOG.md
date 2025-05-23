44861b19b03600fe073c9da601e79d14665b7884
Rong Tao <rongtao@cestc.cn>
2025-05-23 08:56:18 +0800

    Add ulpconfig.mk
    apply ulpconfig.mk to all Makefiles for get ulpconfig command path
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- docs/en/CHANGELOG.md
+++ docs/en/CHANGELOG.md
@@ -14,6 +14,7 @@ and this project adheres to
 #### Breaking Changes
 - Add `ULPATCH_LICENSE()` and `ULPATCH_FILE_VERSION=5`.
 #### Added
+- Add ulpconfig.mk.
 - scripts: add verbose.mk.
 - docker: Add build.sh.
 - Ready to use cmake compile tests/hello.
---
+++ src/ulpconfig.mk
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2025 Rong Tao
+ULPCONFIG_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+ULPCONFIG :=
+
+ifndef ULPCONFIG
+  ULPCONFIG := $(shell which ulpconfig 2>/dev/null)
+  ifeq (${ULPCONFIG},)
+    ULPCONFIG := ${ULPCONFIG_DIR}/ulpconfig.sh.in
+  endif
+endif
+
+ifeq ($(wildcard ${ULPCONFIG}),)
+  $(error Not found ulpconfig in any where)
+endif
+
+ifdef DEBUG
+  $(info ULPCONFIG = ${ULPCONFIG})
+endif
