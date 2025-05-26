0ceb27d54c2a4596021e65de72f8033f66d3a155
Rong Tao <rongtao@cestc.cn>
2025-05-26 08:50:57 +0800

    utils: fix compile errors on aarch64
    [  3%] Building C object src/utils/CMakeFiles/ulpatch_utils.dir/id.c.o
    In file included from /home/rongtao/Git/ulpatch/src/utils/id.c:5:
    /home/rongtao/Git/ulpatch/src/utils/../utils/id.h:5:1: error: unknown type name ‘bool’
        5 | bool is_root(const char *prog);
          | ^~~~
    /home/rongtao/Git/ulpatch/src/utils/../utils/id.h:1:1: note: ‘bool’ is defined in header ‘<stdbool.h>’; did you forget to ‘#include <stdbool.h>’?
      +++ |+#include <stdbool.h>
        1 | // SPDX-License-Identifier: GPL-2.0-or-later
    /home/rongtao/Git/ulpatch/src/utils/id.c:7:1: error: unknown type name ‘bool’
        7 | bool is_root(const char *prog)
          | ^~~~
    /home/rongtao/Git/ulpatch/src/utils/id.c:6:1: note: ‘bool’ is defined in header ‘<stdbool.h>’; did you forget to ‘#include <stdbool.h>’?
        5 | #include "utils/id.h"
      +++ |+#include <stdbool.h>
        6 |
    /home/rongtao/Git/ulpatch/src/utils/id.c: In function ‘is_root’:
    /home/rongtao/Git/ulpatch/src/utils/id.c:10:24: error: ‘true’ undeclared (first use in this function)
       10 |                 return true;
          |                        ^~~~
    /home/rongtao/Git/ulpatch/src/utils/id.c:10:24: note: ‘true’ is defined in header ‘<stdbool.h>’; did you forget to ‘#include <stdbool.h>’?
    /home/rongtao/Git/ulpatch/src/utils/id.c:10:24: note: each undeclared identifier is reported only once for each function it appears in
    /home/rongtao/Git/ulpatch/src/utils/id.c:11:16: error: ‘false’ undeclared (first use in this function)
       11 |         return false;
          |                ^~~~~
    /home/rongtao/Git/ulpatch/src/utils/id.c:11:16: note: ‘false’ is defined in header ‘<stdbool.h>’; did you forget to ‘#include <stdbool.h>’?
    /home/rongtao/Git/ulpatch/src/utils/id.c:12:1: error: control reaches end of non-void function [-Werror=return-type]
       12 | }
          | ^
    cc1: all warnings being treated as errors
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- src/init.h
+++ src/init.h
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /* Copyright (C) 2022-2025 Rong Tao */
 #pragma once
+#include <stdbool.h>
 #include <sys/types.h>
 
 #ifndef PAGE_SIZE
--- src/utils/id.h
+++ src/utils/id.h
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /* Copyright (C) 2022-2025 Rong Tao */
 #pragma once
+#include <stdbool.h>
 
 bool is_root(const char *prog);
